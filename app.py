8248a5d2484353ace217a73b8fa76c82cec089cf
emvalencaf <edsonmvf@gmail.com>
2025-02-09 01:43:25 -0300

    test: try cors

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[global_settings.FRONTEND_URL],
+    allow_origins=["*"],
     allow_headers=["*"],
     allow_methods=["*"],
     allow_credentials=True,
